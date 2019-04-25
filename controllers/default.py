e5b538503d66e4a3cfb6a17b1db68c3d017a5dc7
Beneba <afetsib@gmail.com>
2019-04-25 05:54:28 -0700

    details added

--- controllers/default.py
+++ controllers/default.py
@@ -21,23 +21,41 @@ def store():
 def lecturers():
     return dict()
 
-def tutors():
-    return dict()
-# def store():
-#     submitted_name = request.vars.name
-#     submitted_email = request.vars.email
-#     submitted_password =request.vars.password
 
-#     results = db.users.insert(
-#         db_name =submitted_name,
-#         db_email =submitted_email,
-#         db_password = submitted_password
-#     )
+def store():
+    submitted_surname = request.vars.surname
+    submitted_othername = request.vars.othername
+    submitted_dateofbirth = request.vars.dateofbirth
+    submitted_gender = request.vars.gender
+    submitted_department = request.vars.department 
+    submitted_qualification = request.vars.qualification
+    submitted_email = request.vars.email
+    submitted_number =request.vars.number
+    submitted_staffid = request.vars.staffid
+    submitted_rank = request.vars.rank
 
-#     if results:
-#         return "User Saved Successfully"
-#     else:
-#         return "An Error Occured"
+    results = db.users.insert(
+        db_surname =submitted_surname,
+        db_othername =submitted_othername,
+        db_dateofbirth =submitted_dateofbirth,
+        db_gender =submitted_gender,
+        db_department =submitted_department,
+        db_qualification =submitted_qualification,
+        db_email =submitted_email,
+        db_number =submitted_number,
+        db_staffid =submitted_staffid,
+        db_rank =submitted_rank
+    )
+
+    if results:
+        return "User Saved Successfully"
+    else:
+        return "An Error Occured"
+
+def details():
+   users = db().select(db.users.ALL)
+   return dict(users=users)
+   
 
 def authenticate():
     submitted_email = request.vars.email
--- models/db.py
+++ models/db.py
@@ -5,12 +5,12 @@ db = DAL("sqlite://storage.sqlite")
 db.define_table("users",
             Field('db_name'),
             Field('db_email'),
-            Field('db_password')
-            # Field('db_surname'),
-            # Field('db_othername'),
-            # Field('db_dateofbirth').
-            # Field('db_gender'),
-            # Field('db_department'),
-            # Field('db_qualification'),
-            # Field('db_rank')
+            Field('db_password'),
+            Field('db_surname'),
+            Field('db_othername'),
+            Field('db_dateofbirth').
+            Field('db_gender'),
+            Field('db_department'),
+            Field('db_qualification'),
+            Field('db_rank')
             )
